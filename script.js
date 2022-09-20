c40d806c178e05093348ddfd7d4b32566ad3e914
João Marcos <joaomarcos77777.jm@gmail.com>
2022-09-20 00:57:12 -0300

    toques finais de responsividade,troca de fotos do carousel e adição de JS

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+function notif(){
+  window.alert('Obrigado pelo Comentário!');
+  const comment=document.getElementsByTagName('textarea').value='';
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+
+function filter() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("ul");
+  li = ul.getElementsByTagName("li");
+
+
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (!input.value) {
+          li[i].style.display = "none";
+      } else if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "block";
+      } else {
+          li[i].style.display = "none";
+      }
+
+  }
+}
